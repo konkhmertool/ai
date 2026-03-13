7a0029f0ba9800acf02f52e22baa07cf5bca9778
Konkhmer <lenicolegabriel@gmail.com>
2026-03-13 13:18:15 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 $(function () {
 
-  // global only
+  $("#menuToggle").on("click", function () {
+    $("#mainMenu").toggleClass("show");
+  });
+
+  $(document).on("click", function (e) {
+    if (!$(e.target).closest("#menuToggle, #mainMenu").length) {
+      $("#mainMenu").removeClass("show");
+    }
+  });
 
 });
